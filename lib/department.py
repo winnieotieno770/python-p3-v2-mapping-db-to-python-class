fd52dc50876da3fe71d7d1945d924a3dee178272
winnieotieno770 <winnieotieno770@gmail.com>
2026-01-15 12:43:53 +0300

    Implement Department model with Identity Map and SQLite CRUD

--- lib/department.py
+++ lib/department.py
@@ -2,6 +2,7 @@ from __init__ import CURSOR, CONN
 
 
 class Department:
+    all = {} # dictionary (class attribute) for the objects in our class
 
     def __init__(self, name, location, id=None):
         self.id = id
@@ -42,9 +43,47 @@ class Department:
         """
 
         CURSOR.execute(sql, (self.name, self.location))
-        CONN.commit()
+        CONN.commit() #save python object into the db
 
         self.id = CURSOR.lastrowid
+        # conver the tuple(row data in the db to dict list)
+        type(self).all[self.id] = self #self is class (Department) seif.id is id of our instance object all is the class attribute a dictionary for storing all the instance objects and self.id is the key used to identify a particular instace or object
+
+    @classmethod
+    def instance_from_db(cls,row):
+        """Return a Department object having the attribute values from the table row."""
+        department = cls.all.get(row[0])
+
+        if department:
+            department.name = row[1]
+            department.location = row[2]                      
+        else:
+            department = cls(row[1], row[2])
+            department.id = row[0]
+            cls.all[department.id] = department
+
+        return department
+     
+    @classmethod
+    def get_all(cls):
+        sql = """SELECT * FROM departments"""
+        rows= CURSOR.execute(sql).fetchall() #returns tupples
+        return [cls.instance_from_db(row) for row in rows] # tupple are conevrted to objects
+    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """SELECT * FROM departments WHERE id = ? """
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        object = cls.instance_from_db(row) if row  else print("no record found")
+        return object
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """SELECT * FROM departments 
+                 WHERE name = ?
+        """
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        object = cls.instance_from_db(row) if row else print("no department with that name exist's")
+        return object
 
     @classmethod
     def create(cls, name, location):
@@ -70,5 +109,11 @@ class Department:
             WHERE id = ?
         """
 
+
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
+
+
+        # Delete the dictionary entry using id as the key
+        del type(self).all[self.id]
+        self.id = None
